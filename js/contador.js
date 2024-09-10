091fdbd384c2e1b3b902cbbcadc371268e267e52
Hugo de Lelis <hugodelelis00@gmail.com>
2024-09-10 16:26:03 -0300

    add format in count

--- js/contador.js
+++ js/contador.js
@@ -2,6 +2,10 @@ let count = localStorage.getItem('accessCount') || 0;
 
 count++;
 
+function formatNumber(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
 localStorage.setItem('accessCount', count);
 
-document.getElementById('access-counter').innerText = count;
+document.getElementById('access-counter').innerHTML = formatNumber(count);
