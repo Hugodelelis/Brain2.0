95fcf209529e0f2056b3a5b8debcd87b7e33be52
Hugo de Lelis <hugodelelis00@gmail.com>
2024-09-18 15:18:18 -0300

    test

--- js/login.js
+++ js/login.js
@@ -7,8 +7,8 @@ function exibirAlerta(tipo, mensagem) {
     let alerta = document.createElement('div');
     alerta.classList.add('alert', `alert-${tipo}`, 'alert-dismissible', 'fade', 'show');
     alerta.setAttribute('role', 'alert');
-    alerta.innerHTML = `
-        <strong>${tipo === 'danger' ? 'Erro!' : 'Sucesso!'}</strong> ${mensagem}
+    alerta.innerHTML = ` 
+        <strong>${tipo === 'success' ? 'Sucesso!' : 'Erro!'}</strong> ${mensagem} 
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     `;
     let referencia = document.querySelector('#title');
@@ -25,39 +25,24 @@ function exibirAlerta(tipo, mensagem) {
     }, 5000);
 }
 
-function inicializarUsuarios() {
-    let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
-    
-    const usuariosPreDefinidos = [
-        { email: 'lucas@gmail.com', user: 'Lucas', pass: '123' },
-        { email: 'hugo@gmail.com', user: 'Hugo', pass: '123' },
-        { email: 'thalles@gmail.com', user: 'Thalles', pass: '123' },
-        { email: 'marcos@gmail.com', user: 'Marcos', pass: '123' },
-        { email: 'caio@gmail.com', user: 'Caio', pass: '123' },
-        { email: 'mylena@gmail.com', user: 'Mylena', pass: '123' }
-    ];
+let botaoLogin = document.getElementById('btnlogin');
 
-    usuarios = [...usuariosPreDefinidos, usuarios];
-}
-
-let botao = document.getElementById('btnlogin');
-
-if (botao) {
-    botao.addEventListener('click', function logar(event) {
+if (botaoLogin) {
+    botaoLogin.addEventListener('click', function(event) {
         event.preventDefault();
 
-        let pegaEmail = document.getElementById('login').value;
-        let pegaSenha = document.getElementById('password').value;
+        let email = document.getElementById('login').value;
+        let senha = document.getElementById('password').value;
 
         let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
-        
-        if (!pegaEmail || !pegaSenha) {
+
+        if (!email || !senha) {
             exibirAlerta('danger', 'Preencha todos os campos!');
             return;
         }
 
         let usuarioEncontrado = usuarios.find(usuario =>
-            usuario.email === pegaEmail && usuario.pass === pegaSenha
+            usuario.email === email && usuario.pass === senha
         );
 
         if (usuarioEncontrado) {
