3cbc62e4f703e2631cbbf0f991cb635b9dcc635e
Hugo De Lelis <hugodelelis05@gmail.com>
2024-11-25 16:50:44 -0300

    add biblioteca

--- js/biblioteca.js
+++ js/biblioteca.js
@@ -1,33 +1,25 @@
 var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 30,
+  spaceBetween: 20, 
   freeMode: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
   breakpoints: {
-
-      320: {
-          slidesPerView: 1,
-      },
-
-      689: {
-          slidesPerView: 2,
-      },
-
-      968: {
-          slidesPerView: 3,
-      },
-
-      1331: {
-          slidesPerView: 4,
-      },
-
-      1582: {
-          slidesPerView: 5,
-      },
-  }
+    320: {
+      slidesPerView: 1,
+    },
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+    1440: {
+      slidesPerView: 5,
+    },
+  },
 });
+
 
 const buttons = document.querySelectorAll();
 buttons.forEach(button => {
