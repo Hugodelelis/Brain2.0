284a7563ceacf9990510474ebcdd690d8f100b39
Hugo De Lelis <hugodelelis05@gmail.com>
2024-11-25 15:02:34 -0300

    add comunidade

--- js/user.js
+++ js/user.js
@@ -1,16 +1,12 @@
 window.addEventListener('load', () => {
     let usuarioLogado = sessionStorage.getItem('usuarioLogado');
     let sair = sessionStorage.getItem('sair');
-    let btnVoluntary = document.getElementById('voluntary')
-
-    if(!usuarioLogado) {
-        btnVoluntary.disabled = true
-    }
 
     if (usuarioLogado) {
         document.querySelectorAll('#user').forEach(function(element) {
             element.innerHTML = usuarioLogado;
         });
+
 
         document.querySelectorAll('#login-drop').forEach(function(element) {
             element.innerHTML = `
@@ -19,8 +15,13 @@ window.addEventListener('load', () => {
             `;
         });
 
+
         document.querySelector('#leave').addEventListener('click', function() {
+
             sessionStorage.setItem('usuarioLogado', '');
+            sessionStorage.setItem('usuarioImagem', ''); 
+            sessionStorage.setItem('sair', ''); 
+            sessionStorage.setItem('usuarioEstaLogado', 'false');
 
             document.querySelectorAll('#user').forEach(function(element) {
                 element.innerHTML = '';
